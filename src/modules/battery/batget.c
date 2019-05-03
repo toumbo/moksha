8585c637d930fb988e9f90b4700bc0a07d5d2f32
Robert Wiley <ylee@bodhilinux.com>
2019-05-03 10:41:47 -0400

    use snprintf for string handling in batget.

--- src/modules/battery/batget.c
+++ src/modules/battery/batget.c
@@ -997,8 +997,7 @@ linux_acpi_init(void)
                   FILE *f;
 
                   if (info->name_length + sizeof("/state") >= sizeof(buf)) continue;
-                  strcpy(buf, info->path);
-                  strcat(buf, "/state");
+                  snprintf(buf, sizeof(buf), "%s/state", info->path);
                   f = fopen(buf, "r");
                   if (f)
                     {
@@ -1026,8 +1025,7 @@ linux_acpi_init(void)
              char buf[PATH_MAX + 6];
              FILE *f;
 
-             strcpy(buf, info->path);
-             strcat(buf, "/info");
+             snprintf(buf, sizeof(buf), "%s/info", info->path);
              f = fopen(buf, "r");
              if (f)
                {
