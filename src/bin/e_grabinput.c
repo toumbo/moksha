4d05987cdd1ee98cf377e134ea64aa8ecde6b1c7
Robert Wiley <ylee@bodhilinux.com>
2019-05-26 05:04:47 -0400

    e_grabinput: do not pass a faulty time

--- src/bin/e_grabinput.c
+++ src/bin/e_grabinput.c
@@ -155,14 +155,14 @@ _e_grabinput_focus_do(Ecore_X_Window win, E_Focus_Method method)
       case E_FOCUS_METHOD_NO_INPUT:
         break;
       case E_FOCUS_METHOD_LOCALLY_ACTIVE:
-        ecore_x_window_focus_at_time(win, ecore_x_current_time_get());
+        ecore_x_window_focus(win);
         ecore_x_icccm_take_focus_send(win, ecore_x_current_time_get());
         break;
       case E_FOCUS_METHOD_GLOBALLY_ACTIVE:
         ecore_x_icccm_take_focus_send(win, ecore_x_current_time_get());
         break;
       case E_FOCUS_METHOD_PASSIVE:
-        ecore_x_window_focus_at_time(win, ecore_x_current_time_get());
+        ecore_x_window_focus(win);
         break;
       default:
         break;
